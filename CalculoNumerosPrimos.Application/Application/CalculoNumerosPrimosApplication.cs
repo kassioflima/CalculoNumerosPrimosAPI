aa6abfd2f9ffc8926b52c2a2f43d2f171ebfe046
kassiofernandeslima <72845184+kassiofernandeslima@users.noreply.github.com>
2021-09-01 13:39:16 -0300

    Criando camada de aplição

---
+++ CalculoNumerosPrimos.Application/Application/CalculoNumerosPrimosApplication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoNumerosPrimos.Application.Application
+{
+    class CalculoNumerosPrimosApplication
+    {
+    }
+}
