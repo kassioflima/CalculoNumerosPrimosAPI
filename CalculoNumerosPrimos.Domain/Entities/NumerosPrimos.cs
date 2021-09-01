63917caa5d1286a455ac001fc45f34f41aee7415
kassiofernandeslima <72845184+kassiofernandeslima@users.noreply.github.com>
2021-09-01 14:11:04 -0300

    Implementando classe NumerosPrimos

--- CalculoNumerosPrimos.Domain/Entities/NumerosPrimos.cs
+++ CalculoNumerosPrimos.Domain/Entities/NumerosPrimos.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CalculoNumerosPrimos.Domain.Entities
+﻿namespace CalculoNumerosPrimos.Domain.Entities
 {
-    class NumerosPrimos
+    public class NumerosPrimos
     {
+        public int NumeroEntrada { get; private set; }
+
+        public string Divisores { get; private set; }
+
+        public string Primos { get; private set; }
+
+        public NumerosPrimos(int numero)
+        {
+            NumeroEntrada = numero;
+        }
+
+        public NumerosPrimos CalcularNumeroPrimos(int numEntrada)
+        {
+            var numerosPrimos = new NumerosPrimos(numEntrada);
+            int divisores = 0;
+
+            for (int numero = 1; numero <= numEntrada; numero++)
+            {
+                if ((numEntrada % numero) == 0)
+                {
+                    Divisores += string.Join(" ", $" {numero} ");
+
+                    for (int j = 1; j <= numero; j++)
+                    {
+                        if ((numero % j) == 0)
+                            divisores++;
+                    }
+
+                    if (divisores == 2 || numero == 1)
+                        Primos += string.Join(" ", $" {numero} ");
+
+                    divisores = 0;
+                }
+            }
+
+            return numerosPrimos;
+        }
     }
 }
