8187980b9504b4eb2f4ba6a0c39da962859b7fdc
kassiofernandeslima <72845184+kassiofernandeslima@users.noreply.github.com>
2021-09-01 15:33:39 -0300

    Criando Classe NumerosPrimosQueryResult

---
+++ CalculoNumerosPrimos.Domain/Queries/NumerosPrimosQueryResult.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculoNumerosPrimos.Domain.Queries
+{
+    public class NumerosPrimosQueryResult
+    {
+        public int NumeroEntrada { get; set; }
+
+        public string Divisores { get; set; }
+
+        public string Primos { get; set; }
+    }
+}
