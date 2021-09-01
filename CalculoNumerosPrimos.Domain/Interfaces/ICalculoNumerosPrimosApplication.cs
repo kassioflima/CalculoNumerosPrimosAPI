4435f9e9fa6650364290debd64f90886cd811a95
kassiofernandeslima <72845184+kassiofernandeslima@users.noreply.github.com>
2021-09-01 15:31:46 -0300

    criando interface ICalculoNumerosPrimosApplication

---
+++ CalculoNumerosPrimos.Domain/Interfaces/ICalculoNumerosPrimosApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoNumerosPrimos.Domain.Interfaces
+{
+    public interface ICalculoNumerosPrimosApplication
+    {
+        
+    }
+}
